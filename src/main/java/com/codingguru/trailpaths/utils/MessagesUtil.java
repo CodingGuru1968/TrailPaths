854def39225ac35d28b656982f63056ce9332a42
CodingGuru1968 <codingguru1968@gmail.com>
2026-07-03 00:06:55 +0100

    Minimessage Fix
    + Removed prefixes from messages.
    + Fixed shading Minimessage issues in pom.xml file.

--- src/main/java/com/codingguru/trailpaths/utils/MessagesUtil.java
+++ src/main/java/com/codingguru/trailpaths/utils/MessagesUtil.java
@@ -11,21 +11,19 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public enum MessagesUtil {
 
-	HELP_TITLE("&c&lTrailPaths &r&7- Help Menu", false),
-	HELP_COMMAND("&c%command% &7- %description%", false),
-	TOGGLE_PATH_ON("&eYou have toggled your trail paths: &aON", true),
-	TOGGLE_PATH_OFF("&eYou have toggled your trail paths: &cOFF", true),
-	RELOAD("&aYou have successfully reloaded all configuration files.", true),
-	NO_PERMISSION("&cYou do not have permission to execute this command.", true),
-	IN_GAME_ONLY("&cThis command can only be executed in game.", true),
-	INCORRECT_USAGE("&cCorrect Usage: %command%", true);
+	HELP_TITLE("&c&lTrailPaths &r&7- Help Menu"),
+	HELP_COMMAND("&c%command% &7- %description%"),
+	TOGGLE_PATH_ON("&eYou have toggled your trail paths: &aON"),
+	TOGGLE_PATH_OFF("&eYou have toggled your trail paths: &cOFF"),
+	RELOAD("&aYou have successfully reloaded all configuration files."),
+	NO_PERMISSION("&cYou do not have permission to execute this command."),
+	IN_GAME_ONLY("&cThis command can only be executed in game."),
+	INCORRECT_USAGE("&cCorrect Usage: %command%");
 
 	private String defaultValue;
-	private boolean usePrefix;
 
-	MessagesUtil(String defaultValue, boolean usePrefix) {
+	MessagesUtil(String defaultValue) {
 		this.defaultValue = defaultValue;
-		this.usePrefix = usePrefix;
 	}
 
 	public String getDefault() {
@@ -34,10 +32,6 @@ public enum MessagesUtil {
 
 	public String getPath() {
 		return this.name();
-	}
-
-	public boolean usePrefix() {
-		return this.usePrefix;
 	}
 
 	@Override
